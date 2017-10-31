65d03f57c0d2a0ccc3a6335b150a7ebbe8272c5f
wangchen <q251058506@163.com>
2017-10-31 14:25:52 +0800

    add TransactionManagement

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -2,12 +2,21 @@ package com;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 @MapperScan(basePackages = "com.**.mapper")
-public class Application {
+public class Application extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
 	}
 }
