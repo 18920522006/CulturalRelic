6fc30760ba4ba66b897815df580cfff5e5cf25df
wangchen <q251058506@163.com>
2017-11-01 16:01:57 +0800

    默认跳转登录页面

--- src/main/java/com/base/config/WebMvcConfig.java
+++ src/main/java/com/base/config/WebMvcConfig.java
@@ -1,13 +1,15 @@
 package com.base.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@Configuration
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("forward:/login/login");
+        registry.addViewController("/").setViewName("forward:/login");
         super.addViewControllers(registry);
     }
 }
