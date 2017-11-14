12caec0e990c7d9634242d8bd4f3d0ddc7a1c22c
wangchen <q251058506@163.com>
2017-11-14 14:55:07 +0800

    增加 cglib 动态代理

---
+++ src/test/java/com/proxy/CglibProxy.java
@@ -0,0 +1,26 @@
+package com.proxy;
+
+import org.mockito.cglib.proxy.Enhancer;
+import org.mockito.cglib.proxy.MethodInterceptor;
+import org.mockito.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+public class CglibProxy implements MethodInterceptor {
+
+    private Enhancer enhancer = new Enhancer();
+
+    public Object getProxy(Class clazz) {
+        enhancer.setSuperclass(clazz);
+        enhancer.setCallback(this);
+        return enhancer.create();
+    }
+
+    @Override
+    public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
+        PerformanceMonitor.begin(obj.getClass().getName()+"."+method.getName());
+        Object result = proxy.invokeSuper(obj, args);
+        PerformanceMonitor.end();
+        return result;
+    }
+}
--- src/test/java/com/proxy/TestForumService.java
+++ src/test/java/com/proxy/TestForumService.java
@@ -5,13 +5,16 @@ import org.junit.Test;
 import java.lang.reflect.Proxy;
 
 public class TestForumService {
+
     public static void main(String[] args){
         ForumService service = new ForumServiceImpl();
         service.removeTopic(10);
         service.removeForum(1012);
     }
 
-    @Test
+    /**
+     * jdk 动态代理
+     */
     public void proxy() {
         ForumService target = new ForumServiceImpl();
         PerformanceHandler handler = new PerformanceHandler(target);
@@ -22,5 +25,16 @@ public class TestForumService {
         );
         proxy.removeForum(10);
         proxy.removeTopic(1012);
+    }
+
+    /**
+     *  cglib 动态代理
+     */
+    public void proxy_() {
+        CglibProxy proxy = new CglibProxy();
+        ForumServiceImpl forumService =
+                (ForumServiceImpl)proxy.getProxy(ForumServiceImpl.class);
+        forumService.removeForum(10);
+        forumService.removeTopic(1012);
     }
 }
