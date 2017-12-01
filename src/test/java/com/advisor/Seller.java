a774b44086ce476a14d9478d572744d9756b3ee6
wangchen <q251058506@163.com>
2017-12-01 12:33:42 +0800

    增加 helloTo(),避免其他切面干扰

--- src/test/java/com/advisor/Seller.java
+++ src/test/java/com/advisor/Seller.java
@@ -4,4 +4,12 @@ public class Seller {
     public void greetTo(String name) {
         System.out.println("seller greet to "+name+"...");
     }
+    public void serveTo(String name) {
+        System.out.println("seller serving "+name+"...");
+        helloTo(name);
+    }
+
+    public void helloTo(String name){
+        System.out.println("seller hello "+name+"...");
+    }
 }
