4bb8313a440ba8e3439919198fb0c5d68339dddb
wangchen <q251058506@163.com>
2017-12-01 14:08:05 +0800

    修改

--- src/test/java/com/advisor/GreetingBeforeAdvice.java
+++ src/test/java/com/advisor/GreetingBeforeAdvice.java
@@ -8,6 +8,9 @@ public class GreetingBeforeAdvice implements MethodBeforeAdvice {
     @Override
     public void before(Method method, Object[] args, Object target) throws Throwable {
         System.out.println(target.getClass().getName()+"."+method.getName());
+        if("setSelfProxy".equals(method.getName())){
+            return;
+        }
         String clientName = (String) args[0];
         System.out.println("你好吗 " + clientName);
     }
