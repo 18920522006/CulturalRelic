d66aefec71f69c02a3665e2e3f19921ae23a8814
wangchen <q251058506@163.com>
2017-12-01 14:08:40 +0800

    增加内置对象

--- src/test/java/com/advisor/Seller.java
+++ src/test/java/com/advisor/Seller.java
@@ -1,14 +1,24 @@
 package com.advisor;
 
-public class Seller {
+import com.aop.BeanSelfProxyAware;
+import org.springframework.stereotype.Component;
+
+public class Seller implements BeanSelfProxyAware<Seller> {
+
+    private Seller seller;
+
+    @Override
+    public void setSelfProxy(Seller seller) {
+        this.seller = seller;
+    }
+
     public void greetTo(String name) {
         System.out.println("seller greet to "+name+"...");
     }
     public void serveTo(String name) {
         System.out.println("seller serving "+name+"...");
-        helloTo(name);
+        seller.helloTo(name);
     }
-
     public void helloTo(String name){
         System.out.println("seller hello "+name+"...");
     }
