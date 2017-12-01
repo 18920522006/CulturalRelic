e525f830583ffc0df6903e96163e6b334e4bb7c6
wangchen <q251058506@163.com>
2017-12-01 11:32:20 +0800

    添加注释

--- src/test/java/com/proxy/jdkproxy/PerformanceHandler.java
+++ src/test/java/com/proxy/jdkproxy/PerformanceHandler.java
@@ -9,10 +9,21 @@ public class PerformanceHandler implements InvocationHandler {
 
     private Object target;
 
+    /**
+     *  传入的实现类 必须为接口的实现类
+     * @param target
+     */
     public PerformanceHandler(Object target) {
         this.target = target;
     }
 
+    /**
+     * @param proxy 代理类(由Proxy派生) @{@link java.lang.reflect.Proxy}
+     * @param method 接口中的方法
+     * @param args 方法参数
+     * @return
+     * @throws Throwable
+     */
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
 
