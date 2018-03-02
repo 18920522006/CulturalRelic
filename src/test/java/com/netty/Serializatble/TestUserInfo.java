a484db10a4abee7d420b553b81e7b2e72850df87
wangchen <251058506@qq.com>
2018-03-02 14:21:08 +0800

    JDK序列化机制和通用二进制编码测试

---
+++ src/test/java/com/netty/Serializatble/TestUserInfo.java
@@ -0,0 +1,27 @@
+package com.netty.Serializatble;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+/**
+ * @author wangchen
+ * @date 2018/3/2 14:14
+ */
+public class TestUserInfo {
+    public static void main(String[] args) throws IOException {
+        UserInfo userInfo = new UserInfo();
+        userInfo.buildUserID(100).buildUserName("Welcome to Netty");
+
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ObjectOutputStream os = new ObjectOutputStream(bos);
+        os.writeObject(userInfo);
+        os.flush();
+        os.close();
+
+        byte[] b = bos.toByteArray();
+        System.out.println("jdk 序列化对象大小为：" + b.length);
+        System.out.println("--------------------------------------------");
+        System.out.println("二级制编码大小为：" + userInfo.codeC().length);
+    }
+}
---
+++ src/test/java/com/netty/Serializatble/UserInfo.java
@@ -0,0 +1,60 @@
+package com.netty.Serializatble;
+
+import io.netty.buffer.ByteBuf;
+
+import java.io.Serializable;
+import java.nio.ByteBuffer;
+
+/**
+ * @author wangchen
+ * @date 2018/3/2 13:58
+ */
+public class UserInfo implements Serializable {
+    /**
+     * 序列号
+     */
+    private static final long serialVersionUID = 4339685136131213990L;
+
+    private String userName;
+
+    private int userID;
+
+    public UserInfo buildUserName(String userName) {
+        this.userName = userName;
+        return this;
+    }
+
+    public UserInfo buildUserID(int userID) {
+        this.userID = userID;
+        return this;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public byte[] codeC() {
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        byte[] value = this.userName.getBytes();
+        buffer.putInt(value.length);
+        buffer.put(value);
+        buffer.putInt(this.userID);
+        buffer.flip();
+        value = null;
+        byte[] result = new byte[buffer.remaining()];
+        buffer.get(result);
+        return result;
+    }
+}
