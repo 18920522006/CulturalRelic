5299d5de97ec504e83b6db9b7125fc4004ec0546
wangchen <251058506@qq.com>
2018-03-09 10:57:38 +0800

    Netty HTTP 文件服务

--- src/test/java/com/netty/http/fileServer/HttpFileServer.java
+++ src/test/java/com/netty/http/fileServer/HttpFileServer.java
@@ -1,4 +1,4 @@
-package com.netty.http;
+package com.netty.http.fileServer;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
--- src/test/java/com/netty/http/fileServer/HttpFileServerHandler.java
+++ src/test/java/com/netty/http/fileServer/HttpFileServerHandler.java
@@ -1,4 +1,4 @@
-package com.netty.http;
+package com.netty.http.fileServer;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
