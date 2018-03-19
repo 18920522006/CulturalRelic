0edbb514fd6a776b4fc11086f81bab429b3f8d9d
wangchen <251058506@qq.com>
2018-03-19 13:55:10 +0800

    过时类

--- src/main/java/com/base/convert/PageInfo.java
+++ src/main/java/com/base/convert/PageInfo.java
@@ -3,7 +3,9 @@ package com.base.convert;
 /**
  *  转换 ext js 的分页属性 和 mapper 的分页属性
  * @param <T>
+ *     ext 组件可以调整发送的字段和属性名称
  */
+@Deprecated
 public class PageInfo<T> extends com.github.pagehelper.PageInfo<T> {
 
     private int page;
