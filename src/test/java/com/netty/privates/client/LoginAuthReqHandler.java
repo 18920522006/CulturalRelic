01f492bd58452f6b84d36a3b1753bfe0d1de459e
wangchen <251058506@qq.com>
2018-03-21 14:27:15 +0800

    握手和安全认证

---
+++ src/test/java/com/netty/privates/client/LoginAuthReqHandler.java
@@ -0,0 +1,56 @@
+package com.netty.privates.client;
+
+import com.netty.privates.MessageType;
+import com.netty.privates.pojo.Header;
+import com.netty.privates.pojo.NettyMessage;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+
+/**
+ * @author wangchen
+ * @date 2018/3/21 13:44
+ */
+public class LoginAuthReqHandler extends ChannelHandlerAdapter {
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        ctx.writeAndFlush(buildLoginReq());
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        NettyMessage message = (NettyMessage) msg;
+        /**
+         * 是否为握手应答消息
+         */
+        if (message.getHeader() != null && message.getHeader().getType() == MessageType.HEARTBEAT_RESP.value()) {
+            byte loginResult = (byte) message.getBody();
+            /**
+             * 约定如果捂手成功为0 否则失败 关闭链路
+             */
+            if (loginResult != (byte) 0) {
+                ctx.close();
+            }
+            ctx.fireChannelRead(msg);
+        } else {
+            ctx.fireChannelRead(msg);
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+
+    }
+
+    /**
+     * 请求握手消息
+     * @return
+     */
+    private NettyMessage buildLoginReq() {
+        NettyMessage message = new NettyMessage();
+        Header header = new Header();
+        message.setHeader(header);
+        header.setType(MessageType.LOGIN_REQ.value());
+        return message;
+    }
+}
---
+++ src/test/java/com/netty/privates/server/LoginAuthRespHandler.java
@@ -0,0 +1,78 @@
+package com.netty.privates.server;
+
+import com.netty.privates.MessageType;
+import com.netty.privates.pojo.Header;
+import com.netty.privates.pojo.NettyMessage;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author wangchen
+ * @date 2018/3/21 14:01
+ */
+public class LoginAuthRespHandler extends ChannelHandlerAdapter {
+    /**
+     * 用来存已经登录的名单
+     */
+    private ConcurrentHashMap<String, Boolean> nodeCheck = new ConcurrentHashMap<>();
+    /**
+     * 白名单
+     */
+    private String[] whiteList = new String[]{"127.0.0.1", "192.168.1,142"};
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        NettyMessage message = (NettyMessage) msg;
+        /**
+         * 处理请求的握手消息，其他消息向下传递
+         */
+        if (message.getHeader() != null
+                && message.getHeader().getType() == MessageType.HEARTBEAT_REQ.value()) {
+            String nodeIndex = ctx.channel().remoteAddress().toString();
+            NettyMessage loginResp = null;
+            /**
+             * 拒绝重复登录
+             */
+            if (nodeCheck.containsKey(nodeIndex)) {
+                loginResp = buildResponse((byte) -1);
+            } else {
+                InetSocketAddress address = (InetSocketAddress) ctx.channel().remoteAddress();
+                String ip = address.getAddress().getHostAddress();
+                boolean isOK = false;
+                for (String WIP : whiteList) {
+                    if (WIP.equals(ip)) {
+                        isOK = true;
+                        break;
+                    }
+                }
+                loginResp =  isOK ? buildResponse((byte) 0) : buildResponse((byte) -1);
+                if (isOK) {
+                    nodeCheck.put(nodeIndex, true);
+                }
+                ctx.writeAndFlush(loginResp);
+            }
+        } else {
+            ctx.fireChannelRead(msg);
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+
+    }
+
+    private NettyMessage buildResponse(byte result) {
+        NettyMessage message = new NettyMessage();
+        Header header = new Header();
+        message.setHeader(header);
+        header.setType(MessageType.LOGIN_RESP.value());
+        message.setBody(result);
+        return message;
+    }
+}
