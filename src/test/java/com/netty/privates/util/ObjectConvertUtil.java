b8db47182ac412fceeb6ecc4c495f2d3e5a92fce
wangchen <251058506@qq.com>
2018-04-03 16:04:08 +0800

    优化

--- src/test/java/com/netty/privates/util/ObjectConvertUtil.java
+++ src/test/java/com/netty/privates/util/ObjectConvertUtil.java
@@ -11,16 +11,22 @@ import java.io.*;
  * @date 2018/3/28 14:45
  */
 public class ObjectConvertUtil {
-    public static RequestFile convert(File file) throws Exception {
-        RequestFile requestFile = new RequestFile();
-        requestFile.setFile(file);
-        requestFile.setFileName(file.getName());
-        requestFile.setFileMd5(MD5FileUtil.getMD5String(file));
-        requestFile.setFileType(new MimetypesFileTypeMap().getContentType(file));
-        /**
-         * 默认起始位置为0
-         */
-        requestFile.setStartPosition(0);
-        return requestFile;
+    public static RequestFile[] convert(File[] files) throws Exception {
+        if (files != null && files.length > 0) {
+            RequestFile[] requestFiles = new RequestFile[files.length];
+            for (int i = 0; i < files.length; i++) {
+                File file = files[0];
+                RequestFile requestFile = new RequestFile();
+                requestFile.setFile(file);
+                requestFile.setFileName(file.getName());
+                requestFile.setFileMd5(MD5FileUtil.getMD5String(file));
+                requestFile.setFileType(new MimetypesFileTypeMap().getContentType(file));
+                requestFile.setStartPosition(0);
+                requestFiles[i] = requestFile;
+            }
+            return requestFiles;
+        } else {
+            return new RequestFile[0];
+        }
     }
 }
